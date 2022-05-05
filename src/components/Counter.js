c216a94b8f4d6af46461892fcf0f40cf4a274a46
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-05 14:52:56 -0300

    Funcionalidad personal y custom de los incrementos, decrementos de a 1 e incrementos y decrementos de numeros mayores a 1, junto con la funcionalidad de mostrar/ocultar el contador. Personal y custom porque lo hice sin mirar el curso

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,45 @@
-import {useSelector, useDispatch, connect} from 'react-redux'
+import { useState } from 'react';
+import {useSelector, useDispatch, connect} from 'react-redux' //el connect no lo usamos porque es para class based components
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const [increaseNumb, setIncreaseNumb] = useState(0)
+  const [inputing, setInputing] = useState(false)
+  const [showCounter, setShowCounter] = useState(true)
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
 
-  const incremHandler = () => {
-    dispatch({type: 'increment'})
+  const incremHandler = (event) => {
+    if (!inputing) {
+      if (event.target.name === 'increase') {
+        dispatch({type: 'increment'})
+      }
+      if (event.target.name === 'decrease') {
+        dispatch({type: 'decrement'})
+      }
+    }
+    if (inputing) {
+      dispatch({type: event.target.name, amount: increaseNumb})
+      setInputing(false)
+    } 
   }
 
-  const decremHandler = () => {
-    dispatch({type: 'decrement'})
+  const inputHandler = (event) => {
+    setInputing(true)
+    setIncreaseNumb(Number(event.target.value))
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    setShowCounter(!showCounter)
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <button onClick={incremHandler}>Increment</button>
-      <button onClick={decremHandler}>Decrement</button>
-      <div className={classes.value}>{counter}</div>
+      <input onChange={inputHandler}></input>
+      <button name='increase' onClick={incremHandler}>Increment</button>
+      <button name='decrease' onClick={incremHandler}>Decrement</button>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/indexStore.js
+++ src/store/indexStore.js
@@ -6,6 +6,16 @@ const counterReducer = (state = {counter: 0}, action) => {
             counter: state.counter +1
         }
     }
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount
+        }
+    }
+    if (action.type === 'decrease') {
+        return {
+            counter: state.counter - action.amount
+        }
+    }
     if (action.type === 'decrement') {
         return {
             counter: state.counter -1
